773a2fec86268206beec9420e29ed08c7e0deccf
TTR0NC0 <Oscarmr632@gmail.com>
2022-04-14 00:48:52 +0200

    not much, just a main for not have an empty repo in github xd. Learning about the project...

---
+++ main.c
@@ -0,0 +1,13 @@
+
+
+#include "philo.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc != 5)
+	{
+		write(1, "Incorrect num of arguments", 26);
+		return (0);
+	}
+	
+}
---
+++ philo.h
@@ -0,0 +1,11 @@
+
+
+# ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <unistd.h>
+
+
+
+# endif
