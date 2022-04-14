8c760f8e212fdda3900b5e23e04e513f66014ac5
vm-testing <omr@ubuntu.(none)>
2022-04-14 13:07:46 -0700

    nothing new

--- main.c
+++ main.c
@@ -4,10 +4,15 @@
 
 int	main(int argc, char **argv)
 {
-	if (argc != 5)
-	{
-		write(1, "Incorrect num of arguments", 26);
-		return (0);
-	}
-	
+	// if (argc != 5)
+	// {
+	// 	write(1, "Incorrect num of arguments", 26);
+	// 	return (0);
+	// }
+	pid_t	pd;
+	int		i = 0;
+
+	//pd = fork();
+	while (1)
+		i++;
 }
--- philo.h
+++ philo.h
@@ -5,7 +5,10 @@
 
 # include <stdio.h>
 # include <unistd.h>
-
-
+# include <stdlib.h>
+# include <string.h>
+# include <signal.h>
+# include <sys/types.h>
+# include <sys/wait.h>
 
 # endif
